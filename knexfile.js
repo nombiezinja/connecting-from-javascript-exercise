7ae4bca773b5493d840efac0c445c4ec6ad0c2bb
Ti Zhang <tizhang8835@gmail.com>
2017-06-21 01:41:27 +0000

    Adds migration

--- knexfile.js
+++ knexfile.js
@@ -1,36 +1,18 @@
+const settings = require("./settings"); // settings.json
 // Update with your config settings.
 
 module.exports = {
 
+
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
     },
     pool: {
       min: 2,
